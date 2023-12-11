6c04f9c4a27ff5950a983cc742d77265b615f84f
ienas <ienasharisah@gmail.com>
2023-12-11 13:44:30 +0700

    update product

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,30 @@ class MyApp extends StatelessWidget {
 }
 
 class ProductDetailPage extends StatelessWidget {
-  final String productName = 'Example Product';
-  final String productDescription = 'This is a sample product description.';
-  final double productPrice = 19.99;
+  final String productName =
+      'BARDI Smart Pet Feeder Otomatis WiFi (Lite Version)';
+  final String productDescription =
+      'Koneksi langsung Wi-Fi ke internet, tidak perlu gateway; Remote control pada Aplikasi BARDI Smart Home melalui 3G / 4G / Wi-Fi; Bekerja dengan Amazon Alexa';
+  final double productPrice = 99.99;
   final String productImageUrl =
-      'https://via.placeholder.com/150'; // Replace with actual image URL
+      'https://down-id.img.susercontent.com/file/id-11134207-7qukz-lk5b0rc65f6y2a';
+  final String productImage2 =
+      'https://down-id.img.susercontent.com/file/id-11134207-7qul0-lk5b0rc640mi76';
+  final String productImage3 =
+      'https://down-id.img.susercontent.com/file/id-11134207-7qul6-lk5b0rc62m22ab';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Detail'),
+        centerTitle: true,
+        title: Text(
+          'Product Detail',
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(47, 47, 62, 1)),
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -38,9 +51,27 @@ class ProductDetailPage extends StatelessWidget {
             Center(
               child: Image.network(
                 productImageUrl,
-                height: 200,
-                width: 200,
+                height: 300,
+                width: 300,
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                    productImage2,
+                    height: 80,
+                    width: 80,
+                  ),
+                ),
+                Image.network(
+                  productImage3,
+                  height: 80,
+                  width: 80,
+                )
+              ],
             ),
             SizedBox(height: 20),
             Text(
@@ -55,7 +86,7 @@ class ProductDetailPage extends StatelessWidget {
               'Price: \$${productPrice.toStringAsFixed(2)}',
               style: TextStyle(
                 fontSize: 18,
-                color: Colors.green,
+                color: const Color.fromARGB(255, 76, 175, 155),
               ),
             ),
             SizedBox(height: 20),
